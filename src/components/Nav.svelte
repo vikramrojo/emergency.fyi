5d694abcc82bc62e317fd40d52d8be31be744bb3
Vikram Babu <atavikram@gmail.com>
2020-03-23 12:53:35 -0600

    Initial commit

---
+++ src/components/Nav.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let segment;
+</script>
+
+<style>
+
+</style>
+
+<nav>
+	<ul>
+		<li><a aria-current='{segment === undefined ? "" : undefined}' href='.'>home</a></li>
+		<li><a aria-current='{segment === "about" ? "" : undefined}' href='about'>about</a></li>
+		<li><a rel=prefetch aria-current='{segment === "blog" ? "" : undefined}' href='blog'>blog</a></li>
+	</ul>
+</nav>
