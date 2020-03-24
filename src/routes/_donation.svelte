b39a4e5bd27f67ad8cf301385b5bb51e598e0dba
Tom Lee <user512@users.noreply.github.com>
2020-03-24 14:06:02 -0700

    Add give-back page
    * Assuming we are going to add a donation link in airtable form,
    only display organizations that has donation link

---
+++ src/routes/_donation.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let organization;
+</script>
+
+<li cell p="6 sm3" shadow="3" bg="white">
+  <h3>
+    <a target='_blank' rel='noopener noreferrer' href='{organization.donation}'>{organization.title}</a>
+  </h3>
+  <p>{organization.description}</p>
+</li>
