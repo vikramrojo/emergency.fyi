bbc5db96cb4a3009ad44b39bfe64ad29f4a9716b
Vikram Babu <atavikram@gmail.com>
2020-03-24 00:44:18 -0600

    logo add

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,10 +3,12 @@
 </script>
 
 <style>
-
+img {
+	height: 40px;
+}
 </style>
 
-<nav p="sm3" px="8" py="4" bg="gray3" flex class="h4">
-	<a mr="6" aria-current='{segment === undefined ? "" : undefined}' href='.'>Emergency FYI</a>
+<nav p="sm3" px="8" bg="gray3" flex class="h4" align="center">
+	<a mr="6" py="2" aria-current='{segment === undefined ? "" : undefined}' href='.'><img src="logo.svg" alt="logo"></a>
 	<a aria-current='{segment === undefined ? "" : undefined}' href='health-depts'>Health Departments</a>
 </nav>
