4ba4d0c937afdfb7272c5b5b8b7fa405d62c63c2
Vikram Babu <atavikram@gmail.com>
2020-03-24 21:11:15 -0600

    footer fix

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,11 @@
+<style>
+img {
+	height: 40px;
+}
+</style>
+
 <footer p="sm3" px="8" py="4" bg="gray3" class="h4" color="gray7" flex>
+	<a mr="6" py="2" href='.'><img src="logo.svg" alt="logo"></a>
   <a href="https://github.com/atav1k/emergency.fyi" mr="6">Built by volunteers.</a>
-  <a href="/https://airtable.com/shr90gmWQCu5K2kbZ" >Submit resource</a>
+  <a href="https://airtable.com/shr90gmWQCu5K2kbZ" >Submit resource</a>
 </footer>
