5d81971c0508c5e660dbf3cc793d6f72cbaca2e9
atav1k <atavikram@gmail.com>
2020-04-02 17:51:20 -0600

    manual edits and fixes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -30,23 +30,25 @@
   <ul grid columns="3" gap="6">
     <li cell span="3">
 			<h1><img src="info-square.svg" icon="72" alt="icon title" class="hide" />Emergency FYI for COVID-19</h1>
-      <h2>Helping you navigate the crisis in <span color="red">116</span> impacted counties.</h2>
-			<p><span chip type="warning" mr="2">Last Updated</span><b>Tuesday, March 31, 2020</b></p>
+      <h2>Helping you navigate the crisis in <span color="red">133</span> impacted counties.</h2>
+			<p><span chip type="warning" mr="2">Last Updated</span><b>Wednesday, April 1, 2020</b></p>
     </li>
 
 
     <li cell>
       <h3>The COVID-19 pandemic is overwhelming capacities of local branches of United States government. EmergencyFYI is a resource that provides county-level information to help you navigate this ongoing crisis, expand your network for volunteering, and provide direct support for local relief efforts. If this is an emergency, contact your insurance provider, primary care physician, or your local county health department.</h3>
-			<span class="h3">Projected COVID-19 Peak <a href="https://covid19.healthdata.org/projections">(Source)</span>
-			<h2>April 15, 2020</h2>
+			<span class="h3">Projected Best Case COVID-19 Peak <a href="https://covid19.healthdata.org/projections">(Source)</span>
+			<h2>April 16, 2020</h2>
     </li>
 		<li cell>
 			<span class="h3">US Cases <a href="https://github.com/nytimes/covid-19-data">(Source)</span>
-			<h1>187962</h1>
+			<h1>214,682</h1>
+			<h3><img src="caret-up.svg" icon="24" alt="icon title" />26,720 Daily Increase</h3>
 		</li>
 		<li cell>
 			<span class="h3">US Deaths <a href="https://github.com/nytimes/covid-19-data">(Source)</a></span>
-			<h1>3631</h1>
+			<h1>4,551</h1>
+			<h3><img src="caret-up.svg" icon="24" alt="icon title" />920 Daily Increase</h3>
 		</li>
   </ul>
 </section>
